75bbf379337141a26bb9a30603df5cbd64258d79
Amal Singh Bhadauria <singhamal07@gmail.com>
2026-07-23 20:52:32 +0530

    Leetcode practice added

--- Python_LeetCode/maximise_active_section_2.py
+++ Python_LeetCode/maximise_active_section_2.py
@@ -1,35 +1,77 @@
+class SegmentTree:
+    def __init__(self, arr):
+        self.n = len(arr)
+        self.arr = arr
+        self.seg = [0] * (self.n << 2)
+        if self.n:
+            self.build(1, 0, self.n - 1)
+    def build(self, p: int, l: int, r: int) -> None:
+        if l == r:
+            self.seg[p] = self.arr[l]
+            return
+        mid = (l + r) >> 1
+        self.build(p << 1, l, mid)
+        self.build(p << 1 | 1, mid + 1, r)
+        self.seg[p] = max(self.seg[p << 1], self.seg[p << 1 | 1])
+    def query(self, L: int, R: int) -> int:
+        if L > R:
+            return 0
+        def _query(p: int, l: int, r: int) -> int:
+            if L <= l and r <= R:
+                return self.seg[p]
+            mid = (l + r) >> 1
+            res = 0
+            if L <= mid:
+                res = max(res, _query(p << 1, l, mid))
+            if R > mid:
+                res = max(res, _query(p << 1 | 1, mid + 1, r))
+            return res
+        return _query(1, 0, self.n - 1)
 class Solution:
     def maxActiveSectionsAfterTrade(
-        self, 
-        s: str, 
-        queries: List[List[int]]
+        self, s: str, queries: List[List[int]]
     ) -> List[int]:
-        totalOnes = s.count('1')
-        def bestTrade(sub: str) -> int:
-            ones = sub.count('1')
-            t = "1" + sub + "1"
-            runs = []
-            i = 0
-            while i < len(t):
-                j = i
-                while j < len(t) and t[j] == t[i]:
-                    j += 1
-                runs.append((t[i], j - i))
-                i = j
-            ans = ones
-            for i in range(1, len(runs) - 1):
-                if (
-                    runs[i][0] == '1'
-                    and runs[i - 1][0] == '0'
-                    and runs[i + 1][0] == '0'
-                ):
-                    gain = runs[i - 1][1] + runs[i + 1][1]
-                    ans = max(ans, ones + gain)
-            return ans
-        result = []
+        n = len(s)
+        cnt1 = s.count("1")
+        zeroBlocks = []
+        blockLeft = []
+        blockRight = []
+        i = 0
+        while i < n:
+            st = i
+            while i < n and s[i] == s[st]:
+                i += 1
+            if s[st] == "0":
+                zeroBlocks.append(i - st)
+                blockLeft.append(st)
+                blockRight.append(i - 1)
+        m = len(zeroBlocks)
+        if (
+            m < 2
+        ):
+            return [cnt1] * len(queries)
+        tmpSum = [zeroBlocks[i] + zeroBlocks[i + 1] for i in range(m - 1)]
+        seg = SegmentTree(tmpSum)
+        ans = []
         for l, r in queries:
-            sub = s[l:r + 1]
-            inside = bestTrade(sub)
-            outside = totalOnes - s[l:r + 1].count('1')
-            result.append(inside + outside)
-        return result
+            i = bisect_left(blockRight, l)
+            j = bisect_right(blockLeft, r) - 1
+            if i > m - 1 or j < 0 or i >= j:
+                ans.append(cnt1)
+                continue
+            firstLen = (
+                blockRight[i] - max(blockLeft[i], l) + 1
+            )
+            lastLen = (
+                min(blockRight[j], r) - blockLeft[j] + 1
+            )
+            if i + 1 == j:
+                bestGain = firstLen + lastLen
+                ans.append(cnt1 + bestGain)
+                continue
+            val1 = firstLen + zeroBlocks[i + 1]
+            val2 = zeroBlocks[j - 1] + lastLen
+            val3 = seg.query(i + 1, j - 2)
+            bestGain = max(val1, val2, val3)
+            ans.append(cnt1 + bestGain)
+        return ans
