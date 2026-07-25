912b8fb21c5b4be42d94ad36842881d4965959b7
Amal Singh Bhadauria <singhamal07@gmail.com>
2026-07-25 22:01:30 +0530

    Python Code added

---
+++ Python_LeetCode/maximum_product.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        first = -1
+        second = -1
+
+        while n > 0:
+            digit = n % 10
+
+            if digit >= first:
+                second = first
+                first = digit
+            elif digit > second:
+                second = digit
+
+            n //= 10
+
+        return first * second
