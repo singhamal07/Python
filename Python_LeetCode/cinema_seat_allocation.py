8651a8004488d086b66f3214c9eff24872a46e5c
Amal Singh Bhadauria <singhamal07@gmail.com>
2026-08-19 20:04:48 +0530

    Minor code Change

--- Python_LeetCode/cinema_seat_allocation.py
+++ Python_LeetCode/cinema_seat_allocation.py
@@ -6,11 +6,9 @@ class Solution:
         for row, seat in reservedSeats:
             if 2 <= seat <= 9:
                 row_mask[row] |= 1 << (seat - 2)
-
-        left = 0b00001111    # seats 2-5
-        mid = 0b00111100     # seats 4-7
-        right = 0b11110000   # seats 6-9
-
+        left = 0b00001111
+        mid = 0b00111100
+        right = 0b11110000
         total = 2 * (n - len(row_mask))
         for mask in row_mask.values():
             if mask & left == 0 and mask & right == 0:
